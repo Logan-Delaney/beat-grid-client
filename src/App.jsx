ed1928871c9a3af8a4ce96d7b50f8badba3133ee
Logan-Delaney <logan_delaney@yahoo.com>
2025-11-15 14:13:23 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="App">
+            <h1>BeatGrid</h1>
+            <p>Drum sequencer coming soon...</p>
+        </div>
+    );
+}
+
+export default App;
